977a787e025e834154b446f366d2f9a295de0b84
eugfl <eugfl@hotmail.com>
2023-03-14 15:23:04 -0300

    Exercicios Map, Filter, Reduce

---
+++ Exercicios JS04/ex04.js
@@ -0,0 +1,17 @@
+const companies = [ 
+    { name: 'Samsung', marketValue: 50, CEO: 'Kim Hyun Suk', foundedOn: 1938 }, 
+    { name: 'Microsoft', marketValue: 415, CEO: 'Satya Natella', foundedOn: 1975 }, 
+    { name: 'Intel', marketValue: 117, CEO: 'Brian Krzanich', foundedOn: 1968 }, 
+    { name: 'Facebook', marketValue: 383, CEO: 'Mark Zuckerberg', foundedOn: 2004 }, 
+    { name: 'spotify', marketValue: 30, CEO: 'Daniel Ek', foundedOn: 2006 }, 
+    { name: 'Apple', marketValue: 845, CEO: 'Tim Cook', foundedOn: 1976 } 
+]
+
+const newCompaies = companies.filter( company =>{
+    if ( company.foundedOn <=1975 ) return false
+    if ( company.marketValue <= 200 ) return false
+    
+    return true
+})
+
+console.log(newCompaies)
